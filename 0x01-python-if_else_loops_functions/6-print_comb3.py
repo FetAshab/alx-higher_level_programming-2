4fe5293730c5c6e1725ebe39f13ebc86cc59bcac
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-06-01 02:02:34 -0700

    update

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for num in range(0, 90):
-        if num % 10 > num / 10:
-                if num != 89:
-                        print("{:02d}, ".format(num), end='')
-                else:
-                        print("{:02d}".format(num))
+for j in range(100):
+    if j % 10 > j / 10:
+        if j != 89:
+            print("{}".format(j), end=", ")
+        else: 
+            print("{}".format(j))      
