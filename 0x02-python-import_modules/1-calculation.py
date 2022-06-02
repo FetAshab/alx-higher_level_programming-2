26129ee03e65fb1920856f997b90c02e1fb8bb38
yunusmohammedjemal <yunusmohammed767@gmail.com>
2022-06-02 02:07:15 -0700

    Create 0-add.py

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ from calculator_1 import add, sub, mul, div
 a = 10
 b = 5
 
-print(add(10, 5))
-print(mul(10, 5))
-print(sub(10, 5))
-print(div(10, 5))
+print("{} + {} = {}".format(a, b, add(a, b)))
+print("{} - {} = {}".format(a, b, sub(a, b)))
+print("{} * {} = {}".format(a, b, mul(a, b)))
+print("{} / {} = {}".format(a, b, div(a, b)))
