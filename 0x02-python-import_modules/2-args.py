7306faf0ef577fb5e8fe2c33d6452b9075165e99
yunusmohammedjemal <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-06-02 15:14:58 +0300

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import sys
 num = len(sys.argv)
-if __name == "__main__"
+if __name == "__main__":
     print("{} arguments:".format(num))
     for i in range(1, num, 1):
         print("{}: {}".format(i, sys.argv[i]))
