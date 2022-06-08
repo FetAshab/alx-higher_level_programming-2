1a1b7df37fc62011dc4c447cae6d3d4f2216aaf6
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-06-09 01:31:01 +0300

    Create  10-best_score.py

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    high_keys = list(a_dictionary.keys())
+    maxi = max(high_keys)
+    return maxi
