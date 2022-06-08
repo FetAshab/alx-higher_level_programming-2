659d1749eb804976a701cfbfcdb9bd248b4bc536
yunus mohammed <99275126+yunusmohammedjemal@users.noreply.github.com>
2022-06-09 01:32:25 +0300

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return None
+        return (None)
+    
     high_keys = list(a_dictionary.keys())
     maxi = max(high_keys)
-    return maxi
+    return (maxi)
